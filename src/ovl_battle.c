3cf65a21d69c7b80a6742a779f1eed7d8c4a95d0
Taciano Dreckmann Perez <taciano.perez@gmail.com>
2026-03-13 17:14:10 +0100

    increased visible ships to 12 per side

--- src/ovl_battle.c
+++ src/ovl_battle.c
@@ -4,6 +4,8 @@
 
 void render_battle_screen(GameState *s) {
     unsigned int i;
+    unsigned char player_ship_columns[3] = { 0, 6, 12 };
+    unsigned char pirate_ship_columns[3] = { 23, 29, 35 };
 
     clear_screen();
 
@@ -11,24 +13,16 @@ void render_battle_screen(GameState *s) {
     print(0, 0, "Haxaco Navy");
     print(0, 1, "Firepower:");
     print_int_right_aligned(15, 1, state.frigates * GUNS_PER_FRIGATE);
-    for (i = 0; i < MIN(state.frigates, 8); i++) {
-        if (i % 2 == 0) {
-            draw_picture_at(SHIP, 0, (2 + i * 2));
-        } else {
-            draw_picture_at(SHIP, 6, (i * 2));
-        }
+    for (i = 0; i < MIN(state.frigates, 12); i++) {
+        draw_picture_at(SHIP, player_ship_columns[i % 3], 2 + ((i / 3) * 4));
     }
 
     // Render pirate fleet
-    print(24, 0, "Pirate Fleet");
-    print(24, 1, "Firepower:");
-    print_int_right_aligned(39, 1, state.frigates * GUNS_PER_FRIGATE);
-    for (i = 0; i < MIN(state.frigates, 8); i++) {
-        if (i % 2 == 0) {
-            draw_picture_at(SHIP_PIRATE, 29, (2 + i * 2));
-        } else {
-            draw_picture_at(SHIP_PIRATE, 35, (i * 2));
-        }
+    print(23, 0, "Pirate Fleet");
+    print(23, 1, "Firepower:");
+    print_int_right_aligned(38, 1, state.frigates * GUNS_PER_FRIGATE);
+    for (i = 0; i < MIN(state.frigates, 12); i++) {
+        draw_picture_at(SHIP_PIRATE, pirate_ship_columns[i % 3], 2 + ((i / 3) * 4));
     }
 
     draw_picture_at(ADMIRAL_PORTRAIT, 0, 20);
