d5903f8958d491323bcd3fd599bbf22abf7b87aa
Taciano Dreckmann Perez <taciano.perez@gmail.com>
2026-03-15 20:17:56 +0100

    removed unnecessary string constant

--- src/strings.c
+++ src/strings.c
@@ -3,7 +3,6 @@
 
 const char STR_INVALID_ANSWER[] = "Invalid answer!";
 const char STR_NOT_ENOUGH_RESOURCES[] = "Not enough resources!";
-const char STR_SIR_WE_LACK_RESOURCES[] = "Sir, we lack resources!";
 const char STR_MAX_FMT[] = "(max %u)";
 const char STR_PER_TURN_MAX_FMT[] = "per turn?";
 const char STR_TRADER_COST[] = "A trader costs 1 lumber, 1 fabric,";
